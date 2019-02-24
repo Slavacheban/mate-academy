f5ae5aef00f3d48d6cf46222b59e59043a9f7ec8
Slavacheban <cheban.sl@mail.ru>
2019-02-24 18:41:04 +0200

    Delete Factorial.java

--- homework1/Factorial.java
+++
@@ -1,9 +0,0 @@
-package homework1;
-
-public class Factorial {
-
-    public static int factorial(int n) {
-        if (n == 1 || n == 0) return 1;
-        else return n * (factorial(n - 1));
-    }
-}
