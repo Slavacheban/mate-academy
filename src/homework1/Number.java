ec0743217bd0af20ea71bc2c1bdd1c6ecbaef64d
Slava <slavacheban22@gmail.com>
2019-02-25 21:41:24 +0200

    Changes for a and b

--- src/homework1/Number.java
+++ src/homework1/Number.java
@@ -3,6 +3,14 @@ package homework1;
 public class Number {
 
     public static double pow(double a, double n) {
-        return Math.pow(a, n);
+        if(n == 0) return 1;
+        else if (n == 1) return a;
+        else {
+            double b = a;
+            for (int i = 1; i < n; i++) {
+                a*=b;
+            }
+            return a;
+        }
     }
 }
