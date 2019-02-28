c58dfbfa56292c07923bfb23a3c88f231f7d7929
Slava <slavacheban22@gmail.com>
2019-02-28 22:19:16 +0200

    3d commit in first branch

--- src/homework3/Car/CarWheel.java
+++ src/homework3/Car/CarWheel.java
@@ -3,5 +3,26 @@ package homework3.Car;
 public class CarWheel {
     private double conditionOfWheel;
 
+    public CarWheel() {
+    }
 
+    public CarWheel(double conditionOfWheel) {
+        this.conditionOfWheel = conditionOfWheel;
+    }
+
+    public double getConditionOfWheel() {
+        return conditionOfWheel;
+    }
+
+    public void updateWheel() {
+        conditionOfWheel = 1;
+    }
+
+    public void tiredWheel(double interest) {
+        conditionOfWheel = (conditionOfWheel > interest/100)? (conditionOfWheel - interest/100) : 0;
+    }
+
+    public void printCondition() {
+        System.out.println("Шины стерты на " + (100 - conditionOfWheel*100) + "%");
+    }
 }
